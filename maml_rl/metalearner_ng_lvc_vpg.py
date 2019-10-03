3ff55c56886db06bc90918d01c601f2a1253219e
Zebang Shen <shenzebang@gmail.com>
2019-10-03 08:08:28 +0800

    Try to implement ng-maml with adam optimizer

--- maml_rl/metalearner_ng_lvc_vpg.py
+++ maml_rl/metalearner_ng_lvc_vpg.py
@@ -24,7 +24,6 @@ def vector_to_named_parameter_like(vector, named_parameter_example):
 
     return params
 
-
 class MetaLearnerNGLVCVPG(object):
     """Meta-learner
 
@@ -77,28 +76,6 @@ class MetaLearnerNGLVCVPG(object):
 
         return loss
 
-    def inner_loss_ng(self, episodes, old_pi=None, params=None):
-        pi = self.policy(episodes.observations, params=params)
-        if old_pi is None:
-            old_pi = detach_distribution(pi)
-
-        values = self.baseline(episodes)
-        advantages = episodes.gae(values, tau=self.tau)
-        advantages = weighted_normalize(advantages, weights=episodes.mask)
-        log_ratio = pi.log_prob(episodes.actions) - old_pi.log_prob(episodes.actions)
-        if log_ratio.dim() > 2:
-            log_ratio = torch.sum(log_ratio, dim=2)
-        ratio = torch.exp(log_ratio)
-
-        loss = -weighted_mean(ratio * advantages, dim=0, weights=episodes.mask)
-
-        mask = episodes.mask
-        if episodes.actions.dim() > 2:
-            mask = mask.unsqueeze(2)
-        kl = weighted_mean(kl_divergence(old_pi, pi), dim=0, weights=mask)
-
-        return torch.mean(loss), torch.mean(kl), old_pi
-
     def adapt_ng(self, episodes, first_order=False, max_kl=1e-3, cg_iters=20, cg_damping=1e-2,
              ls_max_steps=10, ls_backtrack_ratio=0.5):
         """Adapt the parameters of the policy network to a new task, from
@@ -116,63 +93,15 @@ class MetaLearnerNGLVCVPG(object):
         hessian_vector_product = self.hessian_vector_product_ng(episodes,
                                                              damping=cg_damping)
         stepdir = conjugate_gradient(hessian_vector_product, grads,
-                                     cg_iters=cg_iters)
+                                     cg_iters=cg_iters).detach()
 
-        # Compute the Lagrange multiplier
-        # shs = 0.5 * torch.dot(stepdir, hessian_vector_product(stepdir))
-        # lagrange_multiplier = torch.sqrt(shs / max_kl)
+        stepdir_named = vector_to_named_parameter_like(stepdir, self.policy.named_parameters())
+        step_size = 0.1
+        params = OrderedDict()
+        for (name, param) in self.policy.named_parameters():
+            params[name] = param - step_size * stepdir_named[name]
 
-        # Save the old parameters
-        old_params = parameters_to_vector(self.policy.parameters())
-
-        # step_size = 1. / lagrange_multiplier.detach().numpy()
-        step_size = 1
-        params = vector_to_named_parameter_like(old_params - step_size * stepdir.detach(),
-                                                self.policy.named_parameters())
-
-        return params, step_size, stepdir.detach()
-
-    def adapt_trpo(self, episodes, first_order=False, max_kl=1e-3, cg_iters=20, cg_damping=1e-2,
-             ls_max_steps=10, ls_backtrack_ratio=0.5):
-        """Adapt the parameters of the policy network to a new task, from
-        sampled trajectories `episodes`, with a one-step natural gradient update.
-        """
-        # Fit the baseline to the training episodes
-        self.baseline.fit(episodes)
-        # Get the loss on the training episodes
-        old_loss, _, old_pi = self.inner_loss_ng(episodes)
-        # Get the new parameters after a one-step natural gradient update
-        grads = torch.autograd.grad(old_loss, self.policy.parameters())
-        grads = parameters_to_vector(grads)
-
-        # Compute the step direction with Conjugate Gradient
-        hessian_vector_product = self.hessian_vector_product_ng(episodes,
-                                                             damping=cg_damping)
-        stepdir = conjugate_gradient(hessian_vector_product, grads,
-                                     cg_iters=cg_iters)
-
-        # Compute the Lagrange multiplier
-        shs = 0.5 * torch.dot(stepdir, hessian_vector_product(stepdir))
-        lagrange_multiplier = torch.sqrt(shs / max_kl)
-
-        # Save the old parameters
-        old_params = parameters_to_vector(self.policy.parameters())
-
-        # Line search
-        step_size = 1.0 / lagrange_multiplier.detach().numpy()
-        for _ in range(ls_max_steps):
-            params = vector_to_named_parameter_like(old_params - (step_size * stepdir).detach(),
-                                                    self.policy.named_parameters())
-            loss, kl, _ = self.inner_loss_ng(episodes, old_pi=old_pi, params=params)
-            improve = loss - old_loss
-            if (improve.item() < 0.0) and (kl.item() < max_kl):
-                break
-            step_size *= ls_backtrack_ratio
-        else:
-            params = vector_to_named_parameter_like(old_params, self.policy.named_parameters())
-            step_size = 0.0
-
-        return params, step_size, stepdir.detach()
+        return params, step_size, stepdir
 
     def sample(self, tasks, first_order=False):
         """Sample trajectories (before and after the update of the parameters) 
@@ -236,10 +165,10 @@ class MetaLearnerNGLVCVPG(object):
              ls_max_steps=10, ls_backtrack_ratio=0.5):
         ng_grads = []
         for train_episodes, valid_episodes in episodes:
-            params_adapt, step_size, step = self.adapt_ng(train_episodes)
+            params_adapt, step_size, stepdir = self.adapt_ng(train_episodes)
 
             # compute $grad = \nabla_x J^{lvc}(x) at x = \theta - \eta\UM(\theta)
-            self.baseline.fit(valid_episodes)
+            # self.baseline.fit(valid_episodes)
             loss = self.inner_loss_lvc(valid_episodes, params=params_adapt)
             ng_grad_0 = torch.autograd.grad(loss, self.policy.parameters())  # no create graph
             ng_grad_0 = parameters_to_vector(ng_grad_0)
@@ -252,7 +181,7 @@ class MetaLearnerNGLVCVPG(object):
 
             # compute $ng_grad_1 = \nabla^2 J^{lvc}(x) at x = \theta times $F_inv_grad
             # create graph for higher differential
-            self.baseline.fit(train_episodes)
+            # self.baseline.fit(train_episodes)
             loss = self.inner_loss_lvc(train_episodes)
 
             grad = torch.autograd.grad(loss, self.policy.parameters(), create_graph=True)
@@ -263,7 +192,7 @@ class MetaLearnerNGLVCVPG(object):
 
             # compute $ng_grad_2 = the Jocobian of {F(x) U(\theta)} at x = \theta times $F_inv_grad
             hessian_vector_product = self.hessian_vector_product_ng(train_episodes, damping=cg_damping)
-            F_U = hessian_vector_product(step.detach())
+            F_U = hessian_vector_product(stepdir)
             ng_grad_2 = torch.autograd.grad(torch.dot(F_U, F_inv_grad.detach()), self.policy.parameters())
             ng_grad_2 = parameters_to_vector(ng_grad_2)
             ng_grad = ng_grad_0 - step_size * (ng_grad_1 + ng_grad_2)
@@ -288,3 +217,77 @@ class MetaLearnerNGLVCVPG(object):
         div = lr * v_bias_corr / (torch.sqrt(sqr_bias_corr) + eps_stable)
         params_update = params - div
         return params_update
+
+    ####################################################################################################################
+    def compute_ng_gradient_test(self, episodes, max_kl=1e-3, cg_iters=20, cg_damping=1e-2,
+             ls_max_steps=10, ls_backtrack_ratio=0.5):
+        ng_grads = []
+        for train_episodes, valid_episodes in episodes:
+            params_adapt, step_size, _ = self.adapt_ng_test(train_episodes)
+
+            # self.baseline.fit(valid_episodes)
+            loss = self.inner_loss_lvc(valid_episodes, params=params_adapt)
+            ng_grad_0 = torch.autograd.grad(loss, self.policy.parameters())  # no create graph
+            ng_grad_0 = parameters_to_vector(ng_grad_0)
+
+            self.baseline.fit(train_episodes)
+            loss = self.inner_loss_lvc(train_episodes)
+            grad = torch.autograd.grad(loss, self.policy.parameters(), create_graph=True)
+            grad = parameters_to_vector(grad)
+            grad_F_inv_grad = torch.dot(grad, ng_grad_0)
+            ng_grad_1 = torch.autograd.grad(grad_F_inv_grad, self.policy.parameters())
+            ng_grad_1 = parameters_to_vector(ng_grad_1)
+
+            ng_grad = ng_grad_0 - step_size * ng_grad_1
+
+            ng_grad = parameters_to_vector(ng_grad)
+            ng_grads.append(ng_grad.view(len(ng_grad), 1))
+
+        return torch.mean(torch.stack(ng_grads, dim=1), dim=[1, 2])
+
+    def step_test(self, episodes, max_kl=1e-3, cg_iters=10, cg_damping=1e-2,
+             ls_max_steps=10, ls_backtrack_ratio=0.5):
+        """Meta-optimization step (ie. update of the initial parameters), based
+        on Trust Region Policy Optimization (TRPO, [4]).
+        """
+        grads = self.compute_ng_gradient_test(episodes)
+        old_params = parameters_to_vector(self.policy.parameters())
+        update_params = self.adam_step(old_params, grads)
+        vector_to_parameters(update_params, self.policy.parameters())
+
+    def sample_test(self, tasks, first_order=False):
+        """Sample trajectories (before and after the update of the parameters)
+        for all the tasks `tasks`.
+        """
+        episodes = []
+        for task in tasks:
+            self.sampler.reset_task(task)
+            train_episodes = self.sampler.sample(self.policy,
+                gamma=self.gamma, device=self.device)
+
+            params, _, _ = self.adapt_ng_test(train_episodes, first_order=first_order)
+
+            valid_episodes = self.sampler.sample(self.policy, params=params,
+                gamma=self.gamma, device=self.device)
+            episodes.append((train_episodes, valid_episodes))
+        return episodes
+
+    def adapt_ng_test(self, episodes, first_order=False, max_kl=1e-3, cg_iters=20, cg_damping=1e-2,
+             ls_max_steps=10, ls_backtrack_ratio=0.5):
+        """Adapt the parameters of the policy network to a new task, from
+        sampled trajectories `episodes`, with a one-step natural gradient update.
+        """
+        # Fit the baseline to the training episodes
+        self.baseline.fit(episodes)
+        # Get the loss on the training episodes
+        loss_lvc = self.inner_loss_lvc(episodes)
+        # Get the new parameters after a one-step natural gradient update
+        grads = torch.autograd.grad(loss_lvc, self.policy.parameters())
+        # grads = parameters_to_vector(grads)
+
+        step_size = 0.1
+        params = OrderedDict()
+        for (name, param), grad in zip(self.policy.named_parameters(), grads):
+            params[name] = param - step_size*grad
+
+        return params, step_size, grads
